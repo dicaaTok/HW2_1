f5da84528e903c3d141cc58e71eef4f8576e8c43
dicaaTok <dicaaGeek@gmail.com>
2025-04-03 23:23:07 +0600

    повтор 2_1

--- app/src/main/java/com/example/hw2_1/MainActivity.java
+++ app/src/main/java/com/example/hw2_1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.hw2_1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button button = findViewById(R.id.button);
+        EditText editText = findViewById(R.id.edit_text);
+        TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String text = editText.getText().toString();
+               textView.setText(text) ;
+
+            }
+        });
+
+
     }
 }
